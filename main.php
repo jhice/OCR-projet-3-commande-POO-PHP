d5e97b851b99e9608393a745b088866499677930
jhice <jcguinez@gmail.com>
2026-05-08 14:20:27 +0200

    command liste (vide)

--- main.php
+++ main.php
@@ -1,7 +1,16 @@
 <?php
 
+// boucle infinie pour la saisie des commandes
+
 while (true) {
+
+    // lecture de l'entrée utilisateur
     $line = readline("Entrez votre commande : ");
-    echo "Vous avez saisi : $line\n";
+    // echo "Vous avez saisi : $line\n";
+
+    // liste (list)
+    if ($line === "list") {
+        echo "affichage de la liste des contacts\n";
+    }
 }
 
