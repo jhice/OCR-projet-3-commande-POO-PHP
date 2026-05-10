eab882384a4516a8df964593c87169042c241780
jhice <jcguinez@gmail.com>
2026-05-10 21:16:31 +0200

    PDO singleton, .gitignore config.php

--- classes/DBConnect.php
+++ classes/DBConnect.php
@@ -4,26 +4,38 @@
  * classe d'accès à la base de données
  */
 
+require_once('./config.php');
+
 class DBConnect
 {
+    /**
+     * instance de PDO (Singleton)
+     */
+    private static ?PDO $client = null;
+
     /**
      * création du client de connexion à MySql
      */
     public static function getPDO()
     {
         try {
-            // On se connecte à MySQL
-            $client = new PDO(
-                'mysql:host=localhost;dbname=contact;charset=utf8',
-                'contact',
-                'contact',
-                [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
-            );
+            // a-t-on besoin d'instancié la classe PDO ?
+            if (static::$client === null) {
+                // On se connecte à MySQL, on stocke la référence dans la propriété statique $client
+                static::$client = new PDO(
+                    // la configuration vient du fichier config.php
+                    'mysql:host='.DB_HOST.';dbname='.DB_NAME.';charset=utf8', DB_USER, DB_PASSWORD,
+                    // gestion des erreurs
+                    [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION],
+                );
+            }
+            // on aura toujours la même instance de PDO
+            // var_dump(static::$client);
         } catch (Exception $e) {
             // En cas d'erreur, on affiche un message et on arrête tout
             die('Erreur : ' . $e->getMessage());
         }
-
-        return $client;
+        // on retourne l'instance du client PDO
+        return static::$client;
     }
 }
---
+++ config.sample.php
@@ -0,0 +1,7 @@
+<?php
+// fichier à coper dans config.php
+// renseigner la configuration de la base de données
+define("DB_HOST", "localhost");
+define("DB_NAME", "contact");
+define("DB_USER", "contact");
+define("DB_PASSWORD", "contact");
