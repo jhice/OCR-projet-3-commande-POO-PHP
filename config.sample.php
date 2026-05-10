65704ce069c8808c10106f3f4b5533b20bee0011
jhice <jcguinez@gmail.com>
2026-05-10 21:19:10 +0200

    clean config.sample.php

--- config.sample.php
+++ config.sample.php
@@ -1,7 +1,7 @@
 <?php
 // fichier à copier dans config.php
 // renseigner la configuration de la base de données
-define("DB_HOST", "localhost");
-define("DB_NAME", "contact");
-define("DB_USER", "contact");
-define("DB_PASSWORD", "contact");
+define("DB_HOST", "");
+define("DB_NAME", "");
+define("DB_USER", "");
+define("DB_PASSWORD", "");
